a7ee012588d55ce09999ecb42889acc8b6de55d9
Viviane Le <anhlv.fpt@gmail.com>
2024-01-28 00:04:13 +0100

    update all steps

--- 2-model-deployment/web_service/lib/modeling.py
+++
@@ -1,9 +0,0 @@
-# from lib.preprocessing import input_values
-from pickle import load
-
-def get_model(model_path: str):
-
-    with open(model_path, 'rb') as f:
-        model = load(f)
-
-    return model
