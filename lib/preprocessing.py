b37b92a6afaf7645ceffc555003b4d4f8b67e584
unknown <stephanie.-@edu.devinci.fr>
2024-01-27 10:58:06 +0100

    preprocess

--- lib/preprocessing.py
+++ lib/preprocessing.py
@@ -6,11 +6,15 @@ import pandas as pd
 from sklearn.feature_extraction import DictVectorizer
 import pickle
 
-def encode_categorical_cols(df: pd.DataFrame, categorical_cols: List[str] = None) -> pd.DataFrame:
+def encode_cols(df: pd.DataFrame, categorical_cols: List[str] = None, numerical_cols: List[str] = None) -> pd.DataFrame:
     if categorical_cols is None:
-        categorical_cols = ["zip_code", "square_feet", "year_built"]
-    df[categorical_cols] = df[categorical_cols].fillna(-1).astype("int")
+        categorical_cols = ["NEIGHBORHOOD", "building_category", "building_class"]
+    if numerical_cols is None:
+        numerical_cols = ["zip_code", "total_unit", "square_feet", "year_built"]
+    # df[categorical_cols] = df[categorical_cols].fillna(-1).astype("int")
+    df[numerical_cols] = df[numerical_cols].fillna(0).astype("float")
     df[categorical_cols] = df[categorical_cols].astype("str")
+    
     return df
 
 def load_preprocessor(path: str):
