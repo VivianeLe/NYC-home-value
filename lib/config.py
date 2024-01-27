90f413d8d1e084f6fa3585a6420b65babc3474e0
unknown <stephanie.-@edu.devinci.fr>
2024-01-27 10:43:40 +0100

    config

--- lib/config.py
+++ lib/config.py
@@ -1,8 +1,8 @@
 # MODELS
 MODEL_VERSION = "0.0.1"
 MODEL_TYPE = "ensemble"
-PATH_TO_PREPROCESSOR = f"/Users/viviane/Desktop/MLOps/NYC-home-value/saved_pkl/dv/{MODEL_TYPE}-{MODEL_VERSION}.pkl"
-PATH_TO_MODEL = f"/Users/viviane/Desktop/MLOps/NYC-home-value/saved_pkl/model/{MODEL_TYPE}-{MODEL_VERSION}.pkl"
+PATH_TO_PREPROCESSOR = f"C:\Git\Project-1\NYC-home-value\saved_pkl\dv\{MODEL_TYPE}-{MODEL_VERSION}.pkl"
+PATH_TO_MODEL = f"C:\Git\Project-1\NYC-home-value\saved_pkl\model_v\{MODEL_TYPE}-{MODEL_VERSION}.pkl"
 CATEGORICAL_COLS = ["NEIGHBORHOOD", "building_category", "building_class"]
 NUMERICAL_COLS = ["zip_code", "total_unit", "square_feet", "year_built"]
 
