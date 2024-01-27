6042a0a04aeb85ba671fa78e82af16974ae9ff44
unknown <stephanie.-@edu.devinci.fr>
2024-01-27 13:52:30 +0100

    preprocess

--- 2-model-deployment/web_service/preprocessing.py
+++ 2-model-deployment/web_service/preprocessing.py
@@ -10,7 +10,7 @@ def encode_cols(df: pd.DataFrame, categorical_cols: List[str] = None, numerical_
     if categorical_cols is None:
         categorical_cols = ["NEIGHBORHOOD", "building_category", "building_class"]
     if numerical_cols is None:
-        numerical_cols = ["zip_code", "total_unit", "square_feet", "year_built"]
+        numerical_cols = ["zip_code", "square_feet", "year_built"]
     # df[categorical_cols] = df[categorical_cols].fillna(-1).astype("int")
     df[numerical_cols] = df[numerical_cols].fillna(0).astype("float")
     df[categorical_cols] = df[categorical_cols].astype("str")
