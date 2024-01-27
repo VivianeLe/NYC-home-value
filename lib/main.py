4e8c336f0f897ce51007e6b1b1a6a17b2214a40b
Viviane Le <anhlv.fpt@gmail.com>
2024-01-28 00:47:51 +0100

    run FastAPI

--- lib/main.py
+++ lib/main.py
@@ -1,54 +1,44 @@
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI
 from pydantic import BaseModel
-from preprocessing import encode_cols, load_preprocessor
-from lib.modeling import get_model
+from preprocessing import encode_cols
+from modeling import load_pickle
 from sklearn.feature_extraction import DictVectorizer
 from typing import List
 import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator
-from config import PATH_TO_PREPROCESSOR, PATH_TO_MODEL, CATEGORICAL_COLS, NUMERICAL_COLS
+from config import PATH_TO_PREPROCESSOR, PATH_TO_MODEL
 import logging
 
-app = FastAPI()
 logger = logging.getLogger(__name__)
 
+app = FastAPI()
 class InputData(BaseModel):
+    NEIGHBORHOOD: str
+    building_category: str
+    building_class: str
     zip_code: int
-    square_feet: int
-    year_built: int
-
-config = {}
-# Load configuration from config.json
-try:
-    with open("config.py", "r") as config_file:
-        exec(config_file.read(), config)
-except Exception as e:
-    logger.error(f"Error loading configuration: {e}")
+    total_unit: int
+    square_feet: float
+    house_age: float
 
 def run_inference(user_input: List[InputData], dv: DictVectorizer, model: BaseEstimator) -> np.ndarray:
-    try:
-        df = pd.DataFrame([x.dict() for x in user_input])
-        df = encode_cols(df)
-        dicts = df[NUMERICAL_COLS].to_dict(orient="records")
-        X = dv.transform(dicts)
-        y = model.predict(X)
-        return y
-    except Exception as e:
-        logger.error(f"Error during inference: {e}")
-        raise HTTPException(status_code=500, detail="Error during inference")
+    df = pd.DataFrame([x.dict() for x in user_input])
+    df = encode_cols(df)
+    dicts = df.to_dict(orient="records")
+    X = dv.transform(dicts)
+    y = model.predict(X)
+    logger.info(f"Predicted house price: {y} USD")
+    return y
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to the NYC Home value prediction!"}
+    return {"message": "This is NYC House Price Prediction App"}
 
-@app.post("/predict_duration")
-def predict_duration_route(payload: InputData):
-    try:
-        dv = load_preprocessor(PATH_TO_PREPROCESSOR)
-        model = get_model(PATH_TO_MODEL)
-        y = run_inference([payload], dv, model)
-        return {"House_price_value: ": y[0]}
-    except Exception as e:
-        logger.error(f"Error predicting duration: {e}")
-        raise HTTPException(status_code=500, detail="Error predicting duration")
+@app.post("/predict_house_price")
+def predict_house_price_route(payload: InputData):
+    dv = load_pickle(PATH_TO_PREPROCESSOR)
+    model = load_pickle(PATH_TO_MODEL)
+    y = run_inference([payload], dv, model)
+    result = f"Predicted house price: {round(y[0]):,.0f} USD"
+    return {result}
