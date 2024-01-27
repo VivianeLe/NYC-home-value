0ce070382bbf2a11b4d4d730f8f1dc4643184ae5
unknown <stephanie.-@edu.devinci.fr>
2024-01-27 14:28:54 +0100

    try-all-step.ipynb

--- 2-model-deployment/web_service/preprocessing.py
+++ 2-model-deployment/web_service/preprocessing.py
@@ -1,24 +1,15 @@
-from fastapi import Request
-import numpy as np
-from pydantic import BaseModel
+from typing import List
 import pandas as pd
 import pickle
 
-def encode_cols(df: pd.DataFrame, categorical_cols: List[str] = None, numerical_cols: List[str] = None) -> pd.DataFrame:
-    if categorical_cols is None:
-        categorical_cols = ["NEIGHBORHOOD", "building_category", "building_class"]
+def encode_cols(df: pd.DataFrame, numerical_cols: List[str] = None) -> pd.DataFrame:
     if numerical_cols is None:
         numerical_cols = ["zip_code", "square_feet", "year_built"]
+    df[numerical_cols] = df[numerical_cols].fillna(-1).astype("int")
+    df[numerical_cols] = df[numerical_cols].astype("str")
+    return df
 
-    # Check if the specified columns exist in the DataFrame
-    for col in categorical_cols + numerical_cols:
-        if col not in df.columns:
-            raise KeyError(f"Column '{col}' not found in the DataFrame.")
-
-    # Perform encoding and filling operations
-    df[numerical_cols] = df[numerical_cols].fillna(0).astype("float")
-    df[categorical_cols] = df[categorical_cols].astype("str")
-
+    
     return df
 
 def load_preprocessor(path):
