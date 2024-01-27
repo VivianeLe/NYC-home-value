5a21211aa2c994edff77d835a6aeb7249db5716d
Viviane Le <anhlv.fpt@gmail.com>
2024-01-27 17:37:13 +0100

    .

--- lib/config.py
+++ lib/config.py
@@ -1,9 +1,9 @@
 # MODELS
 MODEL_VERSION = "0.0.1"
-MODEL_TYPE = "ensemble"
-PATH_TO_PREPROCESSOR = f"/Users/viviane/Desktop/MLOps/NYC-home-value/saved_pkl/dv/{MODEL_TYPE}-{MODEL_VERSION}.pkl"
-PATH_TO_MODEL = f"/Users/viviane/Desktop/MLOps/NYC-home-value/saved_pkl/model/{MODEL_TYPE}-{MODEL_VERSION}.pkl"
-CATEGORICAL_COLS = ["NEIGHBORHOOD", "building_category", "building_class"]
+MODEL_TYPE = "xgb"
+PATH_TO_PREPROCESSOR = f"/Users/viviane/Desktop/MLOps/NYC-home-value/saved_pkl/dv__{MODEL_TYPE}_v{MODEL_VERSION}.pkl"
+PATH_TO_MODEL = f"/Users/viviane/Desktop/MLOps/NYC-home-value/saved_pkl/model__{MODEL_TYPE}_v{MODEL_VERSION}.pkl"
+CATEGORICAL_COLS = ["BOROUGH", "NEIGHBORHOOD", "building_category", "building_class"]
 NUMERICAL_COLS = ["zip_code", "total_unit", "square_feet", "year_built"]
 
 # MISC
