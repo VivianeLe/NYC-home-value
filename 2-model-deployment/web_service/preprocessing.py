7958de9056f588629c917a55c4bb31a8464a8fc6
unknown <stephanie.-@edu.devinci.fr>
2024-01-27 13:59:48 +0100

    preprocess

--- 2-model-deployment/web_service/preprocessing.py
+++ 2-model-deployment/web_service/preprocessing.py
@@ -1,9 +1,7 @@
 from fastapi import Request
 import numpy as np
 from pydantic import BaseModel
-from typing import List
 import pandas as pd
-from sklearn.feature_extraction import DictVectorizer
 import pickle
 
 def encode_cols(df: pd.DataFrame, categorical_cols: List[str] = None, numerical_cols: List[str] = None) -> pd.DataFrame:
@@ -11,10 +9,16 @@ def encode_cols(df: pd.DataFrame, categorical_cols: List[str] = None, numerical_
         categorical_cols = ["NEIGHBORHOOD", "building_category", "building_class"]
     if numerical_cols is None:
         numerical_cols = ["zip_code", "square_feet", "year_built"]
-    # df[categorical_cols] = df[categorical_cols].fillna(-1).astype("int")
+
+    # Check if the specified columns exist in the DataFrame
+    for col in categorical_cols + numerical_cols:
+        if col not in df.columns:
+            raise KeyError(f"Column '{col}' not found in the DataFrame.")
+
+    # Perform encoding and filling operations
     df[numerical_cols] = df[numerical_cols].fillna(0).astype("float")
     df[categorical_cols] = df[categorical_cols].astype("str")
-    
+
     return df
 
 def load_preprocessor(path):
