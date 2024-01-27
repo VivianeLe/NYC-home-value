e2819c998b11728bdcf4ce038a7f2ce26004d711
unknown <stephanie.-@edu.devinci.fr>
2024-01-27 12:15:32 +0100

    config

--- 2-model-deployment/web_service/config.py
+++ 2-model-deployment/web_service/config.py
@@ -1,8 +1,8 @@
 # MODELS
 MODEL_VERSION = "0.0.1"
 MODEL_TYPE = "xgb"
-PATH_TO_PREPROCESSOR = r"C:\Git\Project-1\NYC-home-value\saved_pkl\dv\{MODEL_TYPE}-{MODEL_VERSION}.pkl"
-PATH_TO_MODEL = r"C:\Git\Project-1\NYC-home-value\saved_pkl\model_v\{MODEL_TYPE}-{MODEL_VERSION}.pkl"
+PATH_TO_PREPROCESSOR = r"C:\Git\Project-1\NYC-home-value\saved_pkl\dv\{MODEL_VERSION}.pkl"
+PATH_TO_MODEL = r"C:\Git\Project-1\NYC-home-value\saved_pkl\model_v\{MODEL_VERSION}.pkl"
 CATEGORICAL_COLS = ["NEIGHBORHOOD", "building_category", "building_class"]
 NUMERICAL_COLS = ["zip_code", "total_unit", "square_feet", "year_built"]
 
